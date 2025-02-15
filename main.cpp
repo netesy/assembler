202547703e10105b4a2b35f31506ec2df8ade957
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2025-02-15 06:42:28 +0400

    Initial commiy

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "assembler.hh"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <input.asm> <output.bin>\n";
+        return 1;
+    }
+
+    Assembler assembler;
+    if (!assembler.assemble(argv[1], argv[2])) {
+        return 1;
+    }
+
+    return 0;
+}
